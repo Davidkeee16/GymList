2c8d577b67c7bd2c9f34321e73da670688418627
David <davidkeee16@gmail.com>
2025-07-08 09:17:35 +0300

    Initial Commit

---
+++ gymList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gymList
+//
+//  Created by David Puksanskis on 08/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
